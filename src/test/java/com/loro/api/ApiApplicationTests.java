836fa6b09b1aac3aadcb9cc062d776008b67b6d0
Michal Piontek <michal.piontek@westwing.pl>
2020-03-21 20:20:26 +0100

    [loro] - initial commit

---
+++ src/test/java/com/loro/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
